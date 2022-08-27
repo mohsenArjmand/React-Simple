fc6a709aad00c3f7317a61dfc19d1561fd17a8a9
Mohsen Arjmand <mohsen.arjmandkhah@gmail.com>
2022-08-27 17:30:07 +0430

    this is my first prj

--- src/5-TodoList.js
+++ src/5-TodoList.js
@@ -4,11 +4,13 @@ export default function TodoList(props) {
     /*this.handleEventDel= ()=> {
 console.log('deelte buttomn')
     }*/
-    console.log(props)
+
+    //console.log(props)
     return (
         <div >
             <hr></hr>
             <span key={'todo' + props.todoprops.id} >{props.todoprops.id} - {props.todoprops.task} </span>
+            <input type="text" key={'text' + props.todoprops.id} value={props.newinput}            />
             <button onClick={props.propshandleEventAdd} key={'add' + props.todoprops.id}> + add </button>
             <button onClick={props.propshandleEventDel}> - del </button>
         </div>
